9893d893bf001222ea30938dfe2cee2cc37e6980
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2024-05-27 12:38:03 +0530

    Revert "assignment 1"
    This reverts commit 9127b05629d2cf68e7f64974c93e82486bb16bb7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WanningAlertComponent } from './wanning-alert/wanning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WanningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
